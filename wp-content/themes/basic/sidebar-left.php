bf8a0ea334ba32b48f8b8eee7d2878d96ac35ddd
Casey Manion <casey@manion.com>
2013-03-07 17:45:06 -0800

    auto-updating-files

--- wp-content/themes/basic/sidebar-left.php
+++ wp-content/themes/basic/sidebar-left.php
@@ -1,5 +1,7 @@
 <div id="sidebar-left">
 
+[paypal-donation]
+
 <script language="javascript" src="http://cache.blogads.com/319864931/feed.js"></script><br/><a href=http://www.blogads.com>blog advertising</a> is good for you
 
 <div id="booklist">
@@ -17,7 +19,7 @@
   <a href="http://www.amazon.com/Cities-Wealth-Nations-Vintage-Jacobs/dp/0394729110/ref=ase_sethrobertand-20/102-8713461-6299310?s=books&v=glance&n=283155&tagActionCode=sethrobertand-20">Cities and the Wealth of Nations</a> by Jane Jacobs</div>
 <div class="bookitem"><img src="http://media.sethroberts.net/blog/images/darkage.jpg" width="100" height="100" /><br />
   <a href="http://www.amazon.com/Dark-Age-Ahead-Jane-Jacobs/dp/1400062322/ref=ase_sethrobertand-20/102-8713461-6299310?s=books&v=glance&n=283155&tagActionCode=sethrobertand-20">Dark Age Ahead</a> by Jane Jacobs</div>
-<h2>My Book</h2>
+  <h2>My Book</h2>
   <div class="bookitem"><img src="http://media.sethroberts.net/blog/images/shangrila.jpg" width="100" height="100" border=0" /><br /><a href="http://www.amazon.com/gp/product/0399153640/ref=ase_sethrobertand-20/102-8713461-6299310?s=books&v=glance&n=283155&tagActionCode=sethrobertand-20">The Shangri-La Diet</a> by Seth Roberts</div>
 </div>
 </div>
